e1c31543df19fca545a8e98b878861a4b661f396
riccardoklinger <riccardo.klinger@geolicious.de>
2015-06-19 01:00:09 +0200

    added initial geocoding of addresses

--- OSMroute.py
+++ OSMroute.py
@@ -27,6 +27,12 @@ import resources_rc
 # Import the code for the dialog
 from OSMroute_dialog import OSMrouteDialog
 import os.path
+from qgis.core import * #to get access to qgis
+import qgis.utils
+#we will need these for getting the ors response and to parse it:
+import requests
+from xml.etree import ElementTree
+import urllib2
 
 
 class OSMroute:
@@ -183,10 +189,66 @@ class OSMroute:
         """Run method that performs all the real work"""
         # show the dialog
         self.dlg.show()
-        # Run the dialog event loop
+        print "let's start"
+        canvas = qgis.utils.iface.mapCanvas()
+        allLayers = canvas.layers()
+        #clear layer list prior calling the dialog
+        self.dlg.start.clear()
+        self.dlg.stop.clear()
+        self.dlg.via.clear()
+        self.dlg.mode.clear()
+        #adding current layers to the dlg
+        # index = 1   
+        self.dlg.mode.setItemText(0,'fastest')
+        self.dlg.mode.setItemText(1,'shortest')
+
+        # self.dlg.layer_extent.setItemData(0,'None_id')
+        # self.dlg.layer_extent.setItemText(0,'None - use keywords!')
+        # for i in allLayers: 
+        #     if i.type() == 0 or i.type() == 1: 
+        #         self.dlg.layer_extent.addItem(i.name())      
+        #         self.dlg.layer_extent.setItemData(index,i.id())
+        #         self.dlg.layer_extent.setItemText(index,i.name())
+        #         index = index +1
+        # self.dlg.precision.clear()
+        # self.dlg.precision.addItem('use user location (slow, accurate)')
+        # self.dlg.precision.addItem('use place location (fast, inaccurate)')
+        
+        # self.dlg.output_file.addItem('no file of tweets needed')
+        # self.dlg.output_file.addItem('file of raw tweets needed')
+        # # Run the dialog event loop
         result = self.dlg.exec_()
+
         # See if OK was pressed
         if result:
+
+            start_address = self.dlg.start.text()
+            stop_address = self.dlg.stop.text()
+            #here comes the geocoding:
+            url = "http://openls.geog.uni-heidelberg.de/testing2015/geocoding"
+            text='<?xml version="1.0" encoding="UTF-8"?><xls:XLS xmlns:xls="http://www.opengis.net/xls" xmlns:sch="http://www.ascc.net/xml/schematron" xmlns:gml="http://www.opengis.net/gml" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/xls http://schemas.opengis.net/ols/1.1.0/LocationUtilityService.xsd" version="1.1"><xls:RequestHeader/><xls:Request methodName="GeocodeRequest" requestID="123456789" version="1.1"><xls:GeocodeRequest><xls:Address countryCode="DE"><xls:freeFormAddress>' + start_address + '</xls:freeFormAddress></xls:Address></xls:GeocodeRequest></xls:Request></xls:XLS>'
+            req = urllib2.Request(url=url, 
+                data=text, 
+                headers={'Content-Type': 'application/xml'})
+            response_start=urllib2.urlopen(req).read()
+            #tidy up response
+            response_start=urllib2.urlopen(req).read()
+            newstr = response_start.replace("\n", "")
+            response_start = newstr.replace("  ", "")
+            xml = ElementTree.fromstring(response_start)
+            for child in xml[1][0]:
+                numberOfHits = child.attrib["numberOfGeocodedAddresses"]
+                print numberOfHits
+            if numberOfHits != "0":
+                start_point=xml[1][0][0][0][0][0].text
+                print start_point
+            #url_stop = "http://openls.geog.uni-heidelberg.de/testing2015/geocoding?FreeFormAdress=" + stop_address + "MaxResponse=20"
+            #payload = {'freeFormAddress': 'Feldtmannstr. 132B, Berlin 13088', 'Address countryCode': 'DE'}
+            #response_stop = requests.get(url_stop)
+            #xml_start = ElementTree.fromstring(response_start.content)
+            #xml_stop = ElementTree.fromstring(response_stop.content)
+
             # Do something useful here - delete the line containing pass and
             # substitute with your code.
+            print "routing finished between " + start_address + " and " + stop_address
             pass
